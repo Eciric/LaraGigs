2a86b6a945b20cdc1514b2e3caa03478bfbaeb6d
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-11-07 18:10:47 +0100

    Initial commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+@include('partials._hero')
+@include('partials._search')
+<h1 class="text-3xl m-4">{{$heading}}</h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+    @foreach($listings as $listing) 
+     <x-listing-card :listing="$listing"/>
+    @endforeach
+</div>
+
+@if(count($listings) == 0)
+<p>No listings found</p>
+@endif
+@endsection
