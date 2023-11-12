e93ae74c18472e0a0003f497b55408fd63b7e3d9
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-11-12 11:08:34 +0100

    Added relationships

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing extends Model
-{
+class Listing extends Model {
     use HasFactory;
 
     protected $fillable = ['title', 'company', 'tags', 'location', 'email', 'description', 'website', 'logo'];
@@ -17,8 +16,13 @@ class Listing extends Model
         }
         if ($filters['search'] ?? false) {
             $query->where('title', 'like', '%' . request('search') . '%')
-                  ->orWhere('description', 'like', '%' . request('search') . '%')
-                  ->orWhere('tags', 'like', '%' . request('search') . '%');
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
+    }
+
+    // Relationship to user
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
